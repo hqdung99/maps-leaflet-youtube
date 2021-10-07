bfea55f9322741f25eca480488309bb3aeea92ce
Huynh Quoc Dung <dunghq4@vingroup.net>
2021-10-07 23:39:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  MapContainer,
+  TileLayer,
+  Polygon
+} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { statesData } from './data';
 import './App.css';
 
-function App() {
+const center = [40.63463151377654, -97.89969605983609];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer
+      center={center}
+      zoom={10}
+      style={{ width: '100vw', height: '100vh' }}
+    >
+      <TileLayer
+        url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=YOUR_KEY"
+        attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+      />
+      {
+        statesData.features.map((state) => {
+          const coordinates = state.geometry.coordinates[0].map((item) => [item[1], item[0]]);
+
+          return (<Polygon
+            pathOptions={{
+              fillColor: '#FD8D3C',
+              fillOpacity: 0.7,
+              weight: 2,
+              opacity: 1,
+              dashArray: 3,
+              color: 'white'
+            }}
+            positions={coordinates}
+            eventHandlers={{
+              mouseover: (e) => {
+                const layer = e.target;
+                layer.setStyle({
+                  dashArray: "",
+                  fillColor: "#BD0026",
+                  fillOpacity: 0.7,
+                  weight: 2,
+                  opacity: 1,
+                  color: "white",
+                })
+              },
+              mouseout: (e) => {
+                const layer = e.target;
+                layer.setStyle({
+                  fillOpacity: 0.7,
+                  weight: 2,
+                  dashArray: "3",
+                  color: 'white',
+                  fillColor: '#FD8D3C'
+                });
+              },
+              click: (e) => {
+
+              }
+            }}
+          />)
+        })
+      }
+    </MapContainer>
   );
 }
-
-export default App;
